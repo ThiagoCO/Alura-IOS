1e60af2668657c98444de47e6740989b5d9cd158
Usuário Convidado <Guest@L2602MICRO11.local>
2018-07-25 20:39:43 -0300

    first commit

---
+++ Projeto/Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Projeto
+//
+//  Created by Usuário Convidado on 25/07/2018.
+//  Copyright © 2018 Usuário Convidado. All rights reserved.
+//
+
+import Foundation
---
+++ Projeto/Model/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  Projeto
+//
+//  Created by Usuário Convidado on 25/07/2018.
+//  Copyright © 2018 Usuário Convidado. All rights reserved.
+//
+
+import Foundation
